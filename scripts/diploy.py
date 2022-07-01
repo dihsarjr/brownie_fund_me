00bf66f80c4dd45ccaa59ebc2b0c232472c69f2f
rashid <mohammed1rashid786@gmail.com>
2022-07-01 19:21:18 +0530

    testAgricatorV3Added

--- scripts/diploy.py
+++ scripts/diploy.py
@@ -6,8 +6,8 @@ from scripts.helpful_scripts import get_accounts
 
 def deploy_fund_me():
     account = get_accounts()
-    if network.show_active() == "development":
-        price_field = config["networks"][network.show_active()]["price_field"]
+    if network.show_active() != "development":
+        price_field = config["networks"][network.show_active()]["eth_usd_price_field"]
      
     fund_me = FundMe.deploy(price_field,{"from": account}, publish_source=True)
     print(f"FundMe contract address: {fund_me.address}")
