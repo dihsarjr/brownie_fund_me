2e4843a9e1cdd26b823666194464c5989e12dc78
rashid <mohammed1rashid786@gmail.com>
2022-07-01 23:49:08 +0530

    bug fixed

--- scripts/diploy.py
+++ scripts/diploy.py
@@ -1,15 +1,23 @@
-from brownie import FundMe, network, config
+from brownie import FundMe, MockV3Aggregator, network, config
 from scripts.helpful_scripts import get_accounts
-
-
 
 
 def deploy_fund_me():
     account = get_accounts()
     if network.show_active() != "development":
         price_field = config["networks"][network.show_active()]["eth_usd_price_field"]
-     
-    fund_me = FundMe.deploy(price_field,{"from": account}, publish_source=True)
+    else:
+        print(f"The network is {network.show_active()}")
+        print("deploying fund me on the development network")
+        moke_aggregator = MockV3Aggregator.deploy(
+            18, 2000000000000000000000, {"from": account}
+        )
+        price_field = moke_aggregator.address
+    fund_me = FundMe.deploy(
+        price_field,
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify"),
+    )
     print(f"FundMe contract address: {fund_me.address}")
 
 
