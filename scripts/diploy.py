fb8f8f476b7c02804599b9ae36c4b9010bd808ea
rashid <mohammed1rashid786@gmail.com>
2022-07-01 17:43:03 +0530

    new

--- scripts/diploy.py
+++ scripts/diploy.py
@@ -1,10 +1,17 @@
-from brownie import FundMe
+from brownie import FundMe, network, config
 from scripts.helpful_scripts import get_accounts
+
+
+
 
 def deploy_fund_me():
     account = get_accounts()
-    fund_me = FundMe.deploy({'from': account})
+    if network.show_active() == "development":
+        price_field = config["networks"][network.show_active()]["price_field"]
+     
+    fund_me = FundMe.deploy(price_field,{"from": account}, publish_source=True)
     print(f"FundMe contract address: {fund_me.address}")
 
+
 def main():
-    deploy_fund_me() 
+    deploy_fund_me()
